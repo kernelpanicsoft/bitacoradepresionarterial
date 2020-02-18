f8573cc64a66e1488fe695dbc25f51c9b86f664b
Eduardo Corona <32782870+kernelpanicsoft@users.noreply.github.com>
2020-02-18 17:00:14 -0600

    Test commit from work

---
+++ app/src/main/java/com/ga/kps/bitacoradepresionarterial/RemainderDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.ga.kps.bitacoradepresionarterial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RemainderDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remainder_details)
+    }
+}
---
+++ app/src/main/java/com/ga/kps/bitacoradepresionarterial/RemindersAdapter.kt
@@ -0,0 +1,4 @@
+package com.ga.kps.bitacoradepresionarterial
+
+class RemindersAdapter {
+}
